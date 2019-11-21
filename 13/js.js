f463ba364845cad578a80247ca451280bfe4b5e2
EugeneZaharchenko <eugenezaharchenko@gmail.com>
2019-11-21 09:32:56 +0200

    Experiments with FS module. first steps

---
+++ 13/js.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+const path = './try_node';
+
+// fs.rmdir('try');
+
+if (!(fs.existsSync(path))) {
+    try {
+        fs.mkdir(path, {recursive: true}, (err) => {
+            if (err) throw err;
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+try{
+    fs.writeFile(path + "/file.tmp", "data", function(err){
+        if(err)throw err;
+
+        fs.rename(path + "/file.tmp", path + "/new.tmp", function(err){
+            if(err) throw err;
+        });
+
+    });
+}catch(err){
+    console.error(err);
+}
+
+if(fs.existsSync(path + "/new.tmp")) {
+    try{
+        fs.appendFile(path + "/new.tmp", 'data to append', (err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+        // fs.unlink("new.tmp", function(err)
+    }catch(err){
+        console.error(err);
+    }
+}
+
