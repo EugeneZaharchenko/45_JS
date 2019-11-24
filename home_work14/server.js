b5d3e88c55252c730911e710571092d853254052
EugeneZaharchenko <eugenezaharchenko@gmail.com>
2019-11-24 23:12:25 +0200

    /about - pic added

--- home_work14/server.js
+++ home_work14/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, resp) {
     if (req.url == '/') {
@@ -8,5 +9,10 @@ http.createServer(function (req, resp) {
         console.log(req);
         resp.end()
     }
+    else if (req.url == '/contact') {
+        let file = fs.readFileSync('index.html');
+        resp.end(file)
+    }
     resp.end()
+
 }).listen(3000);
