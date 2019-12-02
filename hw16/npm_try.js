b4de6ea60f46fd282c6dbc90111aace9accda797
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2019-12-02 22:04:17 +0200

    callbacks - not working. Do not understand

--- hw16/npm_try.js
+++ hw16/npm_try.js
@@ -12,28 +12,17 @@ log(chalk.green(
     ' green again!'
 ));
 
-// function createQuote(quote, callback){
-//     var myQuote = "Like I always say, " + quote;
-//     callback(myQuote);
-// }
-//
-// function logQuote(quote){
-//     console.log(quote);
-// }
-//
-// createQuote("eat your vegetables!", logQuote);
-
-// const http = require('http');
-// const request = require('request-promise');
-//
-// function handleError(error, resp) {
-//     resp.writeHead(500, {'Content-Type': 'text/plain'});
-//     resp.end(error.message)
-// }
-
 let request = require('request');
 request('http://quotes.rest/qod.json', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the request contents.
+    let content = JSON.parse(body);
+    return (content["contents"]["quotes"][0]["quote"]) //Return JSON contents
 });
+
+
+let quote = request();
+let message = 'The quote of today is: ';
+console.log(message + quote);
+// console.log(message + function () {
+//                         request()}());
