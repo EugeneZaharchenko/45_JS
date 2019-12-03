21f1de4c74109d35060340dec18261040d3277d4
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2019-12-04 00:29:28 +0200

    simple realization

--- hw16/npm_try.js
+++ hw16/npm_try.js
@@ -17,12 +17,5 @@ request('http://quotes.rest/qod.json', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     let content = JSON.parse(body);
-    return (content["contents"]["quotes"][0]["quote"]) //Return JSON contents
+    console.log(content["contents"]["quotes"][0]["quote"]) //Return JSON contents
 });
-
-
-let quote = request();
-let message = 'The quote of today is: ';
-console.log(message + quote);
-// console.log(message + function () {
-//                         request()}());
