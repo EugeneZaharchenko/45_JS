fa265f4e1ce19a74683c5b09326e07fd6f632f54
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2019-12-17 00:40:27 +0200

    CRUD Express app (courses)

--- hw18/routes/index.js
+++ hw18/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , name: 'Eugene', frame: 'pug' });
+    res.render('index', { title: 'Express' , name: 'Eugene', frame: 'pug' });
 });
 
 module.exports = router;
